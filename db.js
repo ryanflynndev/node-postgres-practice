d295525336ce6a924324a43bfe9fc7e15805c43e
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-27 14:50:20 -0400

    Set up DB pool

--- db.js
+++ db.js
@@ -1,2 +1,11 @@
+//sets config
 const Pool = require("pg").Pool;
 
+const pool = new Pool({
+    user: "ryanflynn";
+    database: "todo_practice_databse",
+    host: "localhost",
+    port: 5432
+})
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
+
+
 
 app.listen(8080, () => {
     console.log("Listening on port 8080")
