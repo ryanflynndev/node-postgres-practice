a7413df4e503731a819e0d7862f44e682411917a
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-27 16:08:42 -0400

    Added update

--- index.js
+++ index.js
@@ -36,6 +36,19 @@ app.post("/todos", async (req, res) => {
     }
 });
 
+app.put("/todos/:id", async(req, res) => {
+    try {
+        const { id } = req.params; //WHERE
+        const { description } = req.body; //SET
+
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0]);
+    } catch(error){
+        console.error(error.message);
+    }
+})
+
 app.listen(8080, () => {
     console.log("Listening on port 8080")
 });
