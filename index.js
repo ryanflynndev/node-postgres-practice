00d5de333d6b529cb46b98762375f027ac2629c0
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-28 11:01:30 -0400

    Made update one query

--- index.js
+++ index.js
@@ -41,9 +41,8 @@ app.put("/todos/:id", async(req, res) => {
         const { id } = req.params; //WHERE
         const { description } = req.body; //SET
 
-        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
-        res.json(todo.rows[0]);
+        const updatedTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *", [description, id]);
+        res.json(updatedTodo.rows[0]);
     } catch(error){
         console.error(error.message);
     }
